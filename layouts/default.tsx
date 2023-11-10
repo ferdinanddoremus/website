32348b28f6e08a735f53a2537badf3687e8d5e53
Grorémus <ferdinanddoremus@gmail.com>
2023-11-10 18:35:08 +0100

    ok

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -4,6 +4,7 @@ import { CommandLineIcon } from '@heroicons/react/24/solid'
 import { useRouter } from 'next/router'
 
 import { BanerComponent } from '@/components/ui/BanerComponent'
+import { Github, Instagram, Linkedin, Twitter } from 'lucide-react'
 
 type CustomProps = {
   children: ReactNode
@@ -14,67 +15,8 @@ const DefaultLayout: FC<CustomProps> = ({ children }) => {
   const { pathname } = useRouter()
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="flex flex-col grow">
-        <div>
-          {children}
-        </div>
-        <footer className="py-10">
-          <div className="container mx-auto px-4">
-            <ul>
-              <li className="mb-2">
-                <Link href="mailto:groremus@icloud.com">
-                  <a className="underline font-bold hover:text-slate-600" target="_blank">
-                    groremus@icloud.com
-                  </a>
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link href="https://github.com/ferdinanddoremus">
-                  <a className="underline font-bold hover:text-slate-600" target="_blank">
-                    github
-                  </a>
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link href="https://twitter.com/Groremus">
-                  <a className="underline font-bold hover:text-slate-600" target="_blank">
-                    twitter
-                  </a>
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link href="https://www.instagram.com/groremus/">
-                  <a className="underline font-bold hover:text-slate-600" target="_blank">
-                    instagram
-                  </a>
-                </Link>
-              </li>
-              <li className="mb-2">
-                <Link href="https://linkedin.com/in/ferdinand-doremus">
-                  <a className="underline font-bold hover:text-slate-600" target="_blank">
-                    linkedin
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </footer>
-      </div>
-      {/* Baner */}
-      {pathname === '/' && showBaner ? (
-        <BanerComponent
-          text="Here is a list a useful tech resources I regularly update."
-          mobileText="My useful tech resources!"
-          cta={
-            <Link href="/resources">
-              <a className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50">View list</a>
-            </Link>
-          }
-          icon={<CommandLineIcon className="h-6 w-6 fill-white" />}
-          onClose={() => setShowBaner(false)}
-        />
-      ) : null}
+    <div className="bg-black flex flex-col min-h-screen">
+      {children}
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import { Github, Instagram, Linkedin, Twitter } from 'lucide-react'
+import { useRef, useState } from 'react'
+import _ from 'lodash'
+import useMouse from 'beautiful-react-hooks/useMouse'
+import useThrottledCallback from 'beautiful-react-hooks/useThrottledCallback'
+
+const lang = ['javascript', 'typescript', 'next', 'react', 'vue', 'nuxt', 'react native', 'swift', 'node.js']
 
 const Home: NextPage = () => {
+  const ref = useRef()
+  const [i, set] = useState(0)
+
+  const [position, { onMouseMove }] = useMouse(ref)
+
+  const setIndex = useThrottledCallback(() => {
+    set(_.random(0, lang.length - 1))
+  }, [], 100)
+
+  onMouseMove(() => { setIndex() })
+
   return (
     <>
       <Head>
@@ -10,20 +29,49 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto px-4">
-        <div className="md:grid grid-cols-2 gap-4">
-          <div className="pt-10">
-            <p className="text-[75px] mb-6">✌️</p>
-            <h1 className="font-bold text-4xl">
-              Hi, I&apos;m Ferdinand
-            </h1>
-            <h2 className="text-lg mt-4 text-slate-500">
-              I’m a frontend engineer mainly focused on delivering amazing experience to users, building great products and using JavaScript, React and React Native.
-            </h2>
-            <p className="text-lg mt-2 text-slate-500">
-              I&apos;m currently looking for my next role. Contact me if you want to work together !
-            </p>
+      <main ref={ref} className="container m-auto px-4 text-white flex-1 flex justify-center items-center">
+        <div>
+          <div className="leading-none flex justify-center items-center gap-[8px]">
+            <span className="text-[75px]">🏕️</span>
+            <span className="text-[58px] mt-[28px]">☕</span>
           </div>
+          <p className="text-[75px] text-center font-bold mt-[-20px]">grorémus</p>
+          <p className="text-[20px] text-slate-600 leading-none text-center ">breaking {lang[i]} apps since 2015</p>
+          <Link href="mailto:groremus@icloud.com">
+            <a className="block underline font-bold hover:text-slate-600 mt-6 text-center" target="_blank">
+              groremus@icloud.com
+            </a>
+          </Link>
+          <ul className="flex items-center justify-center gap-3 mt-3 text-slate-600">
+            <li className="mb-2">
+              <Link href="https://github.com/ferdinanddoremus">
+                <a className="underline font-bold hover:text-white" target="_blank">
+                  <Github size={20} />
+                </a>
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link href="https://twitter.com/Groremus">
+                <a className="underline font-bold hover:text-white" target="_blank">
+                  <Twitter size={20} />
+                </a>
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link href="https://www.instagram.com/groremus/">
+                <a className="underline font-bold hover:text-white" target="_blank">
+                  <Instagram size={20} />
+                </a>
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link href="https://linkedin.com/in/ferdinand-doremus">
+                <a className="underline font-bold hover:text-white" target="_blank">
+                  <Linkedin size={20} />
+                </a>
+              </Link>
+            </li>
+          </ul>
         </div>
       </main>
     </>
